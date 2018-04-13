6ba4635f67666b81499b0791f2ec57639e777c64
Matteo Hertel <info@matteohertel.com>
2018-04-13 10:46:58 +0100

    DON'T MERGE progressbar poc

--- native/packages/react-native-bpk-component-text/stories.js
+++ native/packages/react-native-bpk-component-text/stories.js
@@ -1,4 +1,6 @@
-import React from 'react';
+/* eslint-disable */
+
+import React, { Component } from 'react';
 import {
   colorRed500,
   colorBlue700,
@@ -6,65 +8,139 @@ import {
   colorGreen500,
   colorYellow500,
 } from 'bpk-tokens/tokens/base.react.native';
-import { View, Platform } from 'react-native';
+import { View, Platform, ProgressBarAndroid, Text } from 'react-native';
 import { storiesOf } from '@storybook/react-native';
 import CenterDecorator from '../../storybook/CenterDecorator';
 
 import BpkText from './index';
 
+class ProgressBarAdvanced extends Component {
+  constructor(props) {
+    super(props);
+    this.interval;
+    this.state = {
+      progress: 0.01,
+    };
+  }
+  componentDidMount() {
+    this.initInterval();
+  }
+  incrementProgress() {
+    if (parseInt(this.state.progress, 10) >= 1) {
+      console.warn('done');
+      this.cancelInterval();
+    } else {
+      // console.warn(0.55678 * (this.state.progress * this.state.progress) / 2);
+      // console.warn(Math.pow(1 - this.state.progress, 2) * 0.55678);
+      const k = 0.2835678;
+      let progress;
+      if (this.state.progress > 0.99) {
+        progress = 1;
+      } else {
+        progress = this.state.progress +=
+          Math.pow(1 - this.state.progress, 2) * (this.state.progress * k);
+      }
+
+      this.setState({
+        // (target - current)^2)*k
+        progress,
+      });
+    }
+  }
+  initInterval() {
+    this.interval = setInterval(() => this.incrementProgress(), 16);
+  }
+  cancelInterval() {
+    clearInterval(this.interval);
+  }
+  render() {
+    return (
+      <View>
+        <Text>{this.state.progress}</Text>
+        <ProgressBarAndroid
+          indeterminate={false}
+          styleAttr="Horizontal"
+          progress={this.state.progress}
+        />
+      </View>
+    );
+  }
+}
+class ProgressBar extends Component {
+  constructor(props) {
+    super(props);
+    this.interval;
+    this.state = {
+      progress: 0.01,
+    };
+  }
+  componentDidMount() {
+    this.initInterval();
+  }
+  incrementProgress() {
+    if (parseInt(this.state.progress, 10) >= 1) {
+      console.warn('done');
+      this.cancelInterval();
+    } else {
+      this.setState({
+        progress: this.state.progress + 0.02,
+      });
+    }
+  }
+  initInterval() {
+    this.interval = setInterval(() => this.incrementProgress(), 16);
+  }
+  cancelInterval() {
+    clearInterval(this.interval);
+  }
+  render() {
+    return (
+      <ProgressBarAndroid
+        indeterminate={false}
+        styleAttr="Horizontal"
+        progress={this.state.progress}
+      />
+    );
+  }
+}
 storiesOf('react-native-bpk-component-text', module)
   .addDecorator(CenterDecorator)
-  .add('docs:text-styles', () => (
+  .add('default', () => (
+    <ProgressBarAndroid
+      indeterminate={false}
+      styleAttr="Horizontal"
+      progress={0.7}
+    />
+  ))
+  .add('ProgressbarAdvanced', () => (
     <View>
-      <BpkText textStyle="xxl">Flights to Edinburgh</BpkText>
-      <BpkText textStyle="xl">Flights to Edinburgh</BpkText>
-      <BpkText textStyle="lg">Flights to Edinburgh</BpkText>
-      <BpkText textStyle="base">Flights to Edinburgh</BpkText>
-      <BpkText textStyle="sm">Flights to Edinburgh</BpkText>
-      <BpkText textStyle="xs">Flights to Edinburgh</BpkText>
+      <ProgressBarAdvanced />
+      <ProgressBarAdvanced />
+      <ProgressBarAdvanced />
+      <ProgressBarAdvanced />
+      <ProgressBarAdvanced />
+      <ProgressBarAdvanced />
+      <ProgressBarAdvanced />
+      <ProgressBarAdvanced />
+      <ProgressBarAdvanced />
+      <ProgressBarAdvanced />
+      <ProgressBarAdvanced />
+      <ProgressBarAdvanced />
+      <ProgressBarAdvanced />
+      <ProgressBarAdvanced />
+      <ProgressBarAdvanced />
+      <ProgressBarAdvanced />
+      <ProgressBarAdvanced />
+      <ProgressBarAdvanced />
+      <ProgressBarAdvanced />
+      <ProgressBarAdvanced />
+      <ProgressBarAdvanced />
+      <ProgressBarAdvanced />
+      <ProgressBarAdvanced />
     </View>
   ))
-  .add('Bold', () => (
+  .add('Progressbar', () => (
     <View>
-      <BpkText textStyle="xxl" emphasize={Platform.OS === 'android'}>
-        Flights to Edinburgh
-      </BpkText>
-      <BpkText textStyle="xl" emphasize>
-        Flights to Edinburgh
-      </BpkText>
-      <BpkText textStyle="lg" emphasize>
-        Flights to Edinburgh
-      </BpkText>
-      <BpkText textStyle="base" emphasize>
-        Flights to Edinburgh
-      </BpkText>
-      <BpkText textStyle="sm" emphasize>
-        Flights to Edinburgh
-      </BpkText>
-      <BpkText textStyle="xs" emphasize>
-        Flights to Edinburgh
-      </BpkText>
-    </View>
-  ))
-  .add('Colours', () => (
-    <View>
-      <BpkText textStyle="xxl" style={{ color: colorBlue700 }}>
-        Flights to Edinburgh
-      </BpkText>
-      <BpkText textStyle="xl" style={{ color: colorRed500 }}>
-        Flights to Edinburgh
-      </BpkText>
-      <BpkText textStyle="lg" style={{ color: colorGreen500 }}>
-        Flights to Edinburgh
-      </BpkText>
-      <BpkText textStyle="base" style={{ color: colorYellow500 }}>
-        Flights to Edinburgh
-      </BpkText>
-      <BpkText textStyle="sm" style={{ color: colorBlue700 }}>
-        Flights to Edinburgh
-      </BpkText>
-      <BpkText textStyle="xs" style={{ color: colorGray500 }}>
-        Flights to Edinburgh
-      </BpkText>
+      <ProgressBar />
     </View>
   ));
